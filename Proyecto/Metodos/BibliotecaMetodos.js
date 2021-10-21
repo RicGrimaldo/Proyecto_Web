e1ade24a34ac91147cae8820344fd457d14d3028
RicGrimaldo <rgrimaldo2307@gmail.com>
2021-10-20 23:56:27 -0500

    Se han agregado íconos y métodos para leer el localStorage de biblioteca

--- Proyecto/Metodos/BibliotecaMetodos.js
+++ Proyecto/Metodos/BibliotecaMetodos.js
@@ -1,7 +1,7 @@
 const cards = document.getElementById('cards');
 const templateCard = document.getElementById('template-card').content;
 const fragment = document.createDocumentFragment();
-let biblioteca = {};
+let biblioteca = [];
 
 function Manga(titulo, autor, generos, rangoEdad, rutaArchivo, imagenURL, id) {
     this.titulo = titulo;
@@ -16,14 +16,19 @@ function Manga(titulo, autor, generos, rangoEdad, rutaArchivo, imagenURL, id) {
 document.addEventListener('DOMContentLoaded', function() {
     if(localStorage.getItem('biblioteca')) {
         biblioteca = JSON.parse(localStorage.getItem('biblioteca'));
-        cargarDatos();
+        pintarCards();
     } else{
         alert('No se ha agregado ningún manga.');
     }
 });
 
-const cargarDatos = function(){
-    for(var i = 1; biblioteca[i] != null; i++){
-        console.log(biblioteca[i]);
+const pintarCards = function(){
+    for(var i = 0; i < biblioteca.length; i++){
+        templateCard.querySelector('h5').textContent = biblioteca[i].titulo;
+        templateCard.querySelector('img').setAttribute('src', biblioteca[i].imagenURL);
+        templateCard.querySelector('.btn-dark').dataset.id = biblioteca[i].id;
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
     }
+    cards.appendChild(fragment);
 }
