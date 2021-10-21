9260c3b3a4febe44357bd051f9e7bce5d27b8efb
RicGrimaldo <rgrimaldo2307@gmail.com>
2021-10-21 01:15:36 -0500

    Se pueden descargar los mangas, falta deshabilitar el botón, y preguntar si se desea descargar

--- Proyecto/Metodos/BibliotecaMetodos.js
+++ Proyecto/Metodos/BibliotecaMetodos.js
@@ -26,9 +26,37 @@ const pintarCards = function(){
     for(var i = 0; i < biblioteca.length; i++){
         templateCard.querySelector('h5').textContent = biblioteca[i].titulo;
         templateCard.querySelector('img').setAttribute('src', biblioteca[i].imagenURL);
-        templateCard.querySelector('.btn-dark').dataset.id = biblioteca[i].id;
+        templateCard.querySelector('.button').dataset.id = biblioteca[i].id;
         const clone = templateCard.cloneNode(true);
         fragment.appendChild(clone);
     }
     cards.appendChild(fragment);
 }
+
+cards.addEventListener('click', e =>{
+    descargarClick(e);
+});
+
+//  Cuando se le haga click al botón, mandamos todo el elemento padre a setbiblioteca
+const descargarClick = e =>{
+    if(e.target.classList.contains('button')){
+        descargarPDF(e.target.parentElement);
+    }
+    //Detener cualquier posible evento de cards
+    e.stopPropagation();
+};
+
+//  Para capturar los elementos
+const descargarPDF = objeto => {
+    //Buscamos el manga que fue seleccionado
+    let manga = biblioteca.find(manga => manga.titulo === objeto.querySelector('h5').textContent);
+
+    var btn = objeto.querySelector('.button');
+    
+    btn.innerHTML =  `<span class="icon-check"></span>Descargado`;
+    btn.className = "buttonDescargado";
+    
+    btn.setAttribute("href",manga.rutaArchivo);
+    btn.setAttribute("download",manga.titulo);
+
+};
