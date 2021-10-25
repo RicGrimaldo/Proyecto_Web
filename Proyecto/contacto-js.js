e708c671adc346092eb4e3c593422b2b72424c6f
RicGrimaldo <rgrimaldo2307@gmail.com>
2021-10-24 21:33:47 -0500

    Actualizacion

--- Proyecto/contacto-js.js
+++
@@ -1,7 +0,0 @@
-const btnCambiarOscuro = document.getElementById('btnCambiarOscuro');
-
-
-//  Para agregar o quitar la clase dark al body 
-btnCambiarOscuro.addEventListener('click', function(){
-    document.body.classList.toggle('dark');
-});
