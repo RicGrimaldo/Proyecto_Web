7b1a24a639312f9f1bd519b0e83f756cfede394c
RicGrimaldo <rgrimaldo2307@gmail.com>
2021-10-24 21:22:52 -0500

    Se ha agregado el método para cambiar al modo nocturno

--- Proyecto/CarritoMetodos.js
+++ Proyecto/CarritoMetodos.js
@@ -243,7 +243,23 @@ btnVaciarCarrito.addEventListener('click', function(){
     })
 })
 
-//  Para agregar o quitar la clase dark al body 
-// btnCambiarOscuro.addEventListener('click', function(){
-//     document.body.classList.toggle('dark');
-// });
+const btnSwitch = document.querySelector("#switchb");
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+
+    if(document.body.classList.contains('dark')){
+        document.cookie = "dark=true";
+    }else{
+        document.cookie = "dark=false"
+    }
+});
+
+if(document.cookie.includes("dark=true")){
+    document.body.classList.add('dark');
+    btnSwitch.classList.toggle('active');
+}else{
+    document.body.classList.remove('dark');
+}
