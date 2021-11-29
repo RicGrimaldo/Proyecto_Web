8a5a8b791a35341696b1ae422cd76c03b448f6ac
RicGrimaldo <rgrimaldo2307@gmail.com>
2021-11-29 00:40:31 -0500

    Se ha añadido el header corregido a todas las páginas

--- Proyecto/MetodosJS/Principal_Scripts.js
+++ Proyecto/MetodosJS/Principal_Scripts.js
@@ -1,19 +1,8 @@
 const cards = document.getElementById('cards');
 const templateCard = document.getElementById('template-card').content;
 const fragment = document.createDocumentFragment();
-window.addEventListener("resize", anchoPagina);
-window.addEventListener("load", anchoPagina);
 var mangaRecomendado;
 const mangas = [];
-
-function anchoPagina() {
-    if(window.innerWidth <= 991){
-        document.querySelector('.dropdownUsuario').innerHTML = `<img src="imagenesUsuarios/Grimaldo.jpg" width="45px" height="45px" class="d-inline-block align-top rounded-circle" id="imagenPerfil" alt="">`;
-    }
-    else{
-        document.querySelector('.dropdownUsuario').innerHTML = `<img src="imagenesUsuarios/Grimaldo.jpg" width="35px" height="35px" class="d-inline-block align-top rounded-circle" id="imagenPerfil" alt="">Usuario`;
-    }
-}
 
 function Manga(titulo, autor, generos, sinopsis, rutaArchivo, imagenURL, id, precio) {
     this.titulo = titulo;
--- Proyecto/MetodosJS/dark-mode-Header-Footer.js
+++ Proyecto/MetodosJS/dark-mode-Header-Footer.js
@@ -1,4 +1,15 @@
+window.addEventListener("resize", anchoPagina);
+window.addEventListener("load", anchoPagina);
 const btnSwitch = document.querySelector("#switchb");
+
+function anchoPagina() {
+    if(window.innerWidth <= 991){
+        document.querySelector('.dropdownUsuario').innerHTML = `<img src="imagenesUsuarios/Grimaldo.jpg" width="45px" height="45px" class="d-inline-block align-top rounded-circle" id="imagenPerfil" alt="">`;
+    }
+    else{
+        document.querySelector('.dropdownUsuario').innerHTML = `<img src="imagenesUsuarios/Grimaldo.jpg" width="35px" height="35px" class="d-inline-block align-top rounded-circle" id="imagenPerfil" alt="">Usuario`;
+    }
+}
 
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
